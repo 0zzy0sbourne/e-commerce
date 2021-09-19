5233f6cbd5a71043ac599459ae36389265e1c67a
Ozan <cetino19@itu.edu.tr>
2021-09-20 00:26:16 +0300

    improving categories table

--- src/CategoryList.js
+++ src/CategoryList.js
@@ -5,14 +5,20 @@ import "./CategoryList.css"
 function CategoryList() {
     return (
         <div className = "categories-container">
+            
             <h1>Categories</h1>
             <table id="categorytable" class="category-table">
-            <th>Category Name</th>
-            <th>Parent Category</th> 
-            <th>Description</th>
-            <th>Category Photo</th>
+                
+                <th>Category Name</th>
+                <th>Parent Category</th> 
+                <th>Description</th>
+                <th>Category Photo</th>
        
-          </table>
+            </table>
+            <div class="buttons">
+                <button onclick="categoryfunction()" id="categorybutton" type="button">Add Category</button>
+            </div>
+    
         </div>
     )
 }
