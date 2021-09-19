9a2e4b856f45be872906df07a002d18a08ea7899
Ozan <cetino19@itu.edu.tr>
2021-09-19 23:56:34 +0300

    adding category table

--- src/CategoryList.js
+++ src/CategoryList.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import Sidebar from './Sidebar'
+import "./CategoryList.css"
 
 function CategoryList() {
     return (
-        <div>
-           <p>categories</p>
+        <div className = "categories-container">
+            <h1>Categories</h1>
+            <table id="categorytable" class="category-table">
+            <th>Category Name</th>
+            <th>Parent Category</th> 
+            <th>Description</th>
+            <th>Category Photo</th>
+       
+          </table>
         </div>
     )
 }
