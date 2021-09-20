a5eb7909144e46db2b21f95accae5f233fd4b7d7
Ozan <cetino19@itu.edu.tr>
2021-09-20 19:15:00 +0300

    fixing table

--- src/Form.js
+++ src/Form.js
@@ -5,29 +5,29 @@ class Form extends React.Component {
     return (
       <div id="formdiv">
         <h3>Add a new item to the table:</h3>  
-        <form classname= "form" onSubmit={this.props.handleFormSubmit}>
+        <form className="form" onSubmit={this.props.handleFormSubmit}>
           <label htmlFor="categoryname">
           Category Name:
           <input id="categoryname" value={this.props.newCategoryName} 
-            type="text" name="categoryname"
+            type="text" name="categoryName"
             onChange={this.props.handleInputChange} />
           </label>
           <label for="parentcategory">
           Parent Category:
           <input id="parentcategory" value={this.props.newParentCategory} 
-            type="text" name="parentcategory"
+            type="text" name="parentCategory"
             onChange={this.props.handleInputChange} />
           </label>
           <label htmlFor="categorydescription">
           Category Description:
           <input id="categorydescription" value={this.props.newCategoryDescription} 
-            type="text" name="categorydescription"
+            type="text" name="categoryDescription"
             onChange={this.props.handleInputChange} />
           </label>
           <label htmlFor="categoryphoto">
           Category Photo:
           <input id="categoryphoto" value={this.props.newCategoryPhoto} 
-            type="text" name="categoryphoto"
+            type="text" name="categoryPhoto"
             onChange={this.props.handleInputChange} />
           </label>
           <button type="submit" value="Submit">Add Item</button>
